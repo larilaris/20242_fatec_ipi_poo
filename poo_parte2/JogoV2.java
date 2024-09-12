bf4c886565fc4346573b9ea8848f445e8ad9bf45
Ana Clara <ana.rosa31@fatec.sp.gov.br>
2024-09-12 18:26:45 -0300

    feat: versão dos jogos

--- poo_parte2/JogoV2.java
+++ poo_parte2/JogoV2.java
@@ -1,6 +1,19 @@
 public class JogoV2 {
      public static void main(String[] args) {
           var cacador = new Personagem();
+          cacador.nome = "HeroBrine";
           
-     }
+          while (true) { 
+               cacador.cacar();
+               cacador.comer();
+               cacador.dormir();
+               cacador.cacar();
+               cacador.cacar();
+               cacador.cacar();
+               
+              
+          }
+      }
+
 }
+
